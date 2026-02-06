739614b0f9fe55ede3d8aff08bf6483dc26f6367
KaidanLam <kaidanlam@cpp.edu>
2026-02-05 20:23:06 -0800

    Homework#2.cpp

---
+++ Homework/Homework-01/homework#2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+string country;
+cout << "Enter a country: ";
+getline(cin, country);
+
+cout << "WOAH, I LOVE " << country << ". What part of " << country << " do you come from?" << endl;
+
+}
