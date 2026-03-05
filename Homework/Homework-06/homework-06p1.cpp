e7813f814838b6c5c90412d09782e014359ef411
KaidanLam <kaidanlam@cpp.edu>
2026-03-04 22:49:29 -0800

    Homework06

--- Homework/Homework-06/homework-06p1.cpp
+++ Homework/Homework-06/homework-06p1.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
-#include <cstddef>
 
 int main() {
     int data[]  = {1,3,6,7,9};
-    int data1[] = {5,3,3,3,11};
+    int data1[] = {5,3,3,11,15};   
 
-    for (size_t i = 0; i < std::size(data); ++i) {
+    const int n0 = sizeof(data)  / sizeof(data[0]);
+    const int n1 = sizeof(data1) / sizeof(data1[0]);
+
+    if (n0 != n1) {
+        std::cout << "Cannot swap: arrays are different sizes (" << n0 << " vs " << n1 << ")\n";
+        return 0;
+    }
+
+    for (int i = 0; i < n0; ++i) {
         int temp = data[i];
         data[i] = data1[i];
         data1[i] = temp;
     }
 
-    return 0;
+    // show result
+    std::cout << "data:  ";
+    for (int i = 0; i < n0; ++i) std::cout << data[i] << " ";
+    std::cout << "\ndata1: ";
+    for (int i = 0; i < n1; ++i) std::cout << data1[i] << " ";
+    std::cout << "\n";
 }
